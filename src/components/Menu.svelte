6b5c3c402c3841351b9408a3129fecbe62699617
NoNameDeleted <vuvlad7@mail.ru>
2025-02-11 22:57:59 +0300

    test links

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,14 +1,21 @@
-<script>
+<script lang="ts">
     import Button from "./Button.svelte";
     import { popup } from '@telegram-apps/sdk';
+    import { openTelegramLink } from '@telegram-apps/sdk';
 
-    async function openPopup(){
+    function openTGChanel(){
+        if (openTelegramLink.isAvailable()) {
+            openTelegramLink('https://t.me/just_riiin');
+        }
+    }
+    
+    async function openPopup(buttonName: String){
         if (popup.open.isAvailable()) {
         // popup.isOpened() -> false
         const promise = popup.open({
-            title: 'Hello!',
-            message: 'Here is a test message.',
-            buttons: [{ id: 'my-id', type: 'default', text: 'Default text' }],
+            title: 'Ты нажал кнопку ' + buttonName,
+            message: 'Возможно в будущем тут появится функционал',
+            buttons: [{ id: 'my-id', type: 'default', text: 'Понятно' }],
         });
         // popup.isOpened() -> true
         const buttonId = await promise;
@@ -19,9 +26,9 @@
 </script>
 
 <div>
-    <Button value={'Подбросить монетку ведьме'} onclick={openPopup}/>
-    <Button value={'Подписаться'} onclick={openPopup}/>
-    <Button value={'Уйти с территории Ликориса'} onclick={openPopup}/>
+    <Button value={'Подбросить монетку ведьме'} onclick={() => openPopup('доната')}/>
+    <Button value={'Подписаться'} onclick={openTGChanel}/>
+    <Button value={'Уйти с территории Ликориса'} onclick={() => openPopup('выхода')}/>
 </div>
 
 <style>
