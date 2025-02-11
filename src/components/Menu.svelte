2057b487bd2445faee16ecf6061ba9f18b70e8bb
NoNameDeleted <vuvlad7@mail.ru>
2025-02-11 22:41:37 +0300

    popup test

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,11 +1,27 @@
 <script>
     import Button from "./Button.svelte";
+    import { popup } from '@telegram-apps/sdk';
+
+    async function openPopup(){
+        if (popup.open.isAvailable()) {
+        // popup.isOpened() -> false
+        const promise = popup.open({
+            title: 'Hello!',
+            message: 'Here is a test message.',
+            buttons: [{ id: 'my-id', type: 'default', text: 'Default text' }],
+        });
+        // popup.isOpened() -> true
+        const buttonId = await promise;
+        // popup.isOpened() -> false
+        }
+    }
+    
 </script>
 
 <div>
-    <Button value={'Подбросить монетку ведьме'} onclick={() => {}}/>
-    <Button value={'Подписаться'} onclick={() => {}}/>
-    <Button value={'Уйти с территории Ликориса'} onclick={() => {}}/>
+    <Button value={'Подбросить монетку ведьме'} onclick={openPopup}/>
+    <Button value={'Подписаться'} onclick={openPopup}/>
+    <Button value={'Уйти с территории Ликориса'} onclick={openPopup}/>
 </div>
 
 <style>
