5228dc00b995fb6a4ecdc2edbe27baaf04b0aaff
NoNameDeleted <vuvlad7@mail.ru>
2025-02-11 20:38:36 +0300

    afer build

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './components/Counter.svelte';
   import ProfilePicture from './components/ProfilePicture.svelte';
   import Menu from './components/Menu.svelte';
   import MainContent from './components/MainContent.svelte';
